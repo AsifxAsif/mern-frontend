a31b9fe63237263ff59ef00c3fd3f22a4e983518
AsifxAsif <152244911+AsifxAsif@users.noreply.github.com>
2024-09-18 23:17:40 +0600

    first commit

--- src/forms/manage-restaurant-form/DetailsSection.tsx
+++ src/forms/manage-restaurant-form/DetailsSection.tsx
@@ -68,7 +68,7 @@ const DetailsSection = () => {
           <FormItem className="max-w-[25%]">
             <FormLabel>Delivery price (৳)</FormLabel>
             <FormControl>
-              <Input {...field} className="bg-white" placeholder="1.50" />
+              <Input {...field} className="bg-white" placeholder="50" />
             </FormControl>
             <FormMessage />
           </FormItem>
